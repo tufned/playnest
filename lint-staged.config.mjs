fa2517062b4b6d61d7f5c496da4a596a58758afc
Denys Funderat <funderatdenis@gmail.com>
2024-12-01 00:44:16 +0200

    lint-staged config moved to a separate file

---
+++ lint-staged.config.mjs
@@ -0,0 +1,19 @@
+export default {
+  "apps/api/src/**/*.{js,ts}": [
+    "pnpm format",
+    "turbo lint --filter=@playnest/api --",
+  ],
+  "apps/mobile/**/*.{js,jsx,ts,tsx}": [
+    "pnpm format",
+    "turbo lint --filter=@playnest/mobile --",
+  ],
+  "apps/web/**/*.{js,jsx,ts,tsx}": [
+    "pnpm format",
+    "turbo lint --filter=@playnest/web --",
+  ],
+  "packages/utils/src/**/*.{js,ts}": [
+    "pnpm format",
+    "turbo lint --filter=@playnest/utils --",
+  ],
+  "**/*.{json,md,config.js,config.mjs,yaml,yml}": ["prettier --write"],
+};
