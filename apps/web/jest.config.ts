05afae229ba5c8bceea2f6c66f6a21384e4a6e24
Denys Funderat <funderatdenis@gmail.com>
2025-01-03 20:40:40 +0200

    jest installed on api and web

---
+++ apps/web/jest.config.ts
@@ -0,0 +1,24 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+import type { Config } from 'jest';
+import nextJest from 'next/jest';
+
+const createJestConfig = nextJest({
+  dir: './'
+});
+
+const config: Config = {
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  moduleNameMapper: {
+    '^~/(.*)$': '<rootDir>/$1'
+  },
+  testEnvironment: 'jsdom'
+};
+
+export default createJestConfig(config);
