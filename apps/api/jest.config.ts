d53f23e914e1e78c8a5fd8f78c67ca035c6b0079
Denys Funderat <funderatdenis@gmail.com>
2025-01-04 02:06:07 +0200

    jest configured in api + user service unit tests added

--- apps/api/jest.config.ts
+++ apps/api/jest.config.ts
@@ -9,7 +9,18 @@ const config: Config = {
   clearMocks: true,
   collectCoverage: true,
   coverageDirectory: 'coverage',
-  coverageProvider: 'v8'
+  coverageProvider: 'v8',
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+    '@playnest/utils': '<rootDir>/../packages/utils/src'
+  },
+  transform: {
+    '^.+.tsx?$': ['ts-jest', {
+      useESM: true
+    }]
+  },
+  testRegex: '.*\\.(spec|test)\\.(t|j)s$'
 };
 
 export default config;
