f9a978f696248682470d0bbf833b5d8921418133
Denys Funderat <funderatdenis@gmail.com>
2025-02-20 15:37:27 +0200

    @playnest/core export update

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,2 +1,2 @@
-console.log(true);
-console.log(true);
+export * from "./types/index";
+export * from "./configs/index";
---
+++ packages/core/src/types/index.ts
@@ -0,0 +1,2 @@
+export * from "./response.types";
+export * from "./domains/user.types";
