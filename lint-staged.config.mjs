6dffe3f12ae3181211f35585373f3904fcf65044
Denys Funderat <funderatdenis@gmail.com>
2025-02-20 14:05:25 +0200

    lint-stagged fix

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -7,9 +7,13 @@ export default {
     "pnpm format",
     "turbo lint --filter=@playnest/shop --",
   ],
-  "packages/utils/src/**/*.{js,ts}": [
+  "packages/core/src/**/*.{js,ts}": [
     "pnpm format",
-    "turbo lint --filter=@playnest/shared --",
+    "turbo lint --filter=@playnest/core --",
+  ],
+  "packages/web/src/**/*.{js,ts,jsx,tsx}": [
+    "pnpm format",
+    "turbo lint --filter=@playnest/web --",
   ],
   "**/*.{json,md,config.js,config.mjs,yaml,yml}": ["prettier --write"],
 };
