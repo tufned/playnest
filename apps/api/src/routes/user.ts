c5023b618e2efc8e3c0fecff67177758e99ef100
Denys Funderat <funderatdenis@gmail.com>
2025-02-26 15:39:54 +0200

    api user endpoints auth middleware added

--- apps/api/src/routes/user.ts
+++ apps/api/src/routes/user.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
 import asyncDecorator from "../utils/asyncDecorator.js";
 import UserController from "../controllers/UserController.js";
+import authMiddleware from "../middlewares/auth.js";
 
 const userRouter: Router = Router();
+
+userRouter.use(authMiddleware);
 
 userRouter.get("/", asyncDecorator(UserController.getAllUsers));
 userRouter.get("/:id", asyncDecorator(UserController.getUser));
